475234e05b0e5920233e5666c33425413ec608ca
vladmeh <vladmeh@gmail.com>
2018-03-04 03:14:19 +0300

    HW01_1.1

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(MealServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOG.debug("get meals.jsp and forward meals");
+
+        req.getRequestDispatcher("/meals.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: vlad
+  Date: 04.03.2018
+  Time: 2:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Meals</title>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<h2>Meals</h2>
+</body>
+</html>
