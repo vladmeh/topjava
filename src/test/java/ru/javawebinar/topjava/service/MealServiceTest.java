b88597f633d3632e4403850c60190714307e41a3
VladMeh <vladmeh@gmail.com>
2018-03-19 13:58:49 +0300

    HW03_1_SpringMain

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @autor mvl on 19.03.2018.
+ */
+public class MealServiceTest {
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void create() {
+    }
+}
