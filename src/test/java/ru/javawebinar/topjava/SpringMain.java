f23ccb9d5ad554d6fe49aa4ed6846b2172ef9d24
vladmeh <vladmeh@gmail.com>
2018-04-04 01:11:13 +0300

    HW05_fix_user_email_SpringMain

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -25,7 +25,7 @@ public class SpringMain {
 
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
+            adminUserController.create(new User(null, "userName", "email@email.loc", "password", Role.ROLE_ADMIN));
             System.out.println();
 
             MealRestController mealController = appCtx.getBean(MealRestController.class);
