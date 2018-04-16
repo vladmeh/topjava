7eab8838de993fdb29637eeb35fede7bba3197a4
vladmeh <vladmeh@gmail.com>
2018-04-17 01:38:29 +0300

    HW06_1_2_fix_jdbc_tests

--- src/test/java/ru/javawebinar/topjava/service/jpa/JpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaMealServiceTest.java
@@ -1,10 +1,24 @@
 package ru.javawebinar.topjava.service.jpa;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 
+import javax.validation.ConstraintViolationException;
+import java.time.Month;
+
+import static java.time.LocalDateTime.of;
 import static ru.javawebinar.topjava.Profiles.JPA;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ActiveProfiles(JPA)
 public class JpaMealServiceTest extends AbstractMealServiceTest {
+    @Test
+    public void testValidation() throws Exception {
+        validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "  ", 300), USER_ID), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new Meal(null, null, "Description", 300), USER_ID), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Description", 9), USER_ID), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Description", 5001), USER_ID), ConstraintViolationException.class);
+    }
 }
--- src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
@@ -1,10 +1,27 @@
 package ru.javawebinar.topjava.service.jpa;
 
+import org.junit.Test;
+
 import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.AbstractJpaUserServiceTest;
+
+import javax.validation.ConstraintViolationException;
+import java.util.Collections;
+import java.util.Date;
 
 import static ru.javawebinar.topjava.Profiles.JPA;
 
 @ActiveProfiles(JPA)
-public class JpaUserServiceTest extends AbstractUserServiceTest {
+public class JpaUserServiceTest extends AbstractJpaUserServiceTest {
+
+    @Test
+    public void testValidation() throws Exception {
+        validateRootCause(() -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "  ", "password", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 9, true, new Date(), Collections.emptySet())), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 10001, true, new Date(), Collections.emptySet())), ConstraintViolationException.class);
+    }
 }
