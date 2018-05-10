e1e2aaf08aec78e5d137b8e77bb2ae29e3108f01
VladMeh <vladmeh@gmail.com>
2018-05-10 12:20:19 +0300

    HW09_review

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -48,7 +48,6 @@ public class Meal extends AbstractBaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
-    @NotNull
     private User user;
 
     public Meal() {
